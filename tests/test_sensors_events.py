35a653cc24fc59f6bc846baad2355e7c76c3d5b4
Jason Wu <jason@jiliguala.com>
2022-06-09 15:29:12 +0800

    自动获取excel中埋点事件，输出list

--- tests/test_sensors_events.py
+++ tests/test_sensors_events.py
@@ -4,11 +4,25 @@ from garbevents.sensors_events import GetData
 
 from garbevents.settings import Settings as ST
 
-'gb -p 8889 -s test_sensors_events.py'
-ST.url = 'https://sensors.wenwo.com'
+import pandas as pd
+
+'mitmdump -p 8889 -s test_sensors_events.py'
+#  ST.url = 'https://sc.jiligaga.com/sa?project=default'
+ST.url = 'https://sc.jiligaga.com/sa?project=production'
 ST.report_path = 'report'
-ST.all_events = ['LX_CONVERSATION_MSG_ITEM_FORWAR_CLICK', 'LX_CONVERSATION_SETTING_TOPS_CLICK']
+
+
+def excel_one_line_to_list():
+    df = pd.read_excel('/Users/jasonwu/Downloads/cocos埋点大全.xlsx', sheet_name='埋点汇总', header=0, usecols='A',
+                       keep_default_na=False)
+
+    df_li = df.values.tolist()
+    result = []
+    for s_li in df_li:
+        result.append(s_li[0])
+
 
 addons = [
-    GetData()
+    GetData(),
+    excel_one_line_to_list()
 ]
