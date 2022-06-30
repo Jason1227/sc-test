98fea44ec56fb8ec220164eadcf2e7b9b9f89cc5
Jason Wu <jason@jiliguala.com>
2022-06-30 09:48:30 +0800

    update

--- tests/test_custom_sensors_events.py
+++ tests/test_custom_sensors_events.py
@@ -4,16 +4,56 @@ from garbevents.custom_sensors_events import GetData
 
 from garbevents.settings import Settings as ST
 
+import pandas as pd
 
-'gb -p 8889 -s test_custom_sensors_events.py'
-ST.url = 'https://sensors.wenwo.com'
+'mitmdump -p 8889 -s test_custom_sensors_events.py'
+
 ST.report_path = 'report'
-ST.all_events = ['$WebClick', '$pageview']
+
+#  国内版本
+ST.url = 'https://sc.jiliguala.com/sa?project=default'
+#  ST.url = 'https://sc.jiliguala.com/sa?project=production'
+
+ST.report_path = 'report'
+
+
+#  从excel表格中读取埋点事件名
+# def excel_one_line_to_list():
+#     df = pd.read_excel('/Users/jlglqa/Downloads/叽里呱啦核心埋点.xlsx', sheet_name='Sheet1', header=1, usecols='B',
+#                        keep_default_na=False)
+#
+#     df_li = df.values.tolist()
+#     ST.all_events = []
+#     for s_li in df_li:
+#         ST.all_events.append(s_li[0])
+#
+#     print(ST.all_events)
+
+
+#  海外版本
+#  ST.url = 'https://sc.jiligaga.com/sa?project=default'
+#  ST.url = 'https://sc.jiligaga.com/sa?project=production'
+
+
+# def excel_one_line_to_list_intl():
+#     df = pd.read_excel('/Users/jlglqa/Downloads/cocos埋点大全.xlsx', sheet_name='埋点汇总', header=0, usecols='A',
+#                        keep_default_na=False)
+#
+#     df_li = df.values.tolist()
+#     ST.all_events = []
+#     for s_li in df_li:
+#         ST.all_events.append(s_li[0])
+#
+#     print(ST.all_events)
+
+ST.all_events = ['home_view']
+
 ST.events_properties = {
-    '$WebClick': ['app_name', 'is_login', 'part_name', 'platform_type', 'referrer_path'],
-    '$pageview': ['app_name', 'is_login', 'part_name', 'platform_type', 'referrer_path']
+    'home_view': ['vesion'],
 }
 
 addons = [
-    GetData()
+    GetData(),
+    #  excel_one_line_to_list(),
+    #  excel_one_line_to_list_intl(),
 ]
--- tests/test_sensors_events.py
+++ tests/test_sensors_events.py
@@ -8,35 +8,15 @@ import pandas as pd
 
 'mitmdump -p 8889 -s test_sensors_events.py'
 
-
 #  国内版本
-#  ST.url = 'https://sc.jiliguala.com/sa?project=default'
+ST.url = 'https://sc.jiliguala.com/sa?project=default'
 
 
-#  ST.url = 'https://sc.jiliguala.com/sa?project=production'
+# ST.url = 'https://sc.jiliguala.com/sa?project=production'
 
 #  从excel表格中读取埋点事件名
-# def excel_one_line_to_list():
-#     df = pd.read_excel('/Users/jlglqa/Downloads/叽里呱啦核心埋点.xlsx', sheet_name='Sheet1', header=1, usecols='B',
-#                        keep_default_na=False)
-#
-#     df_li = df.values.tolist()
-#     ST.all_events = []
-#     for s_li in df_li:
-#         ST.all_events.append(s_li[0])
-#
-#     print(ST.all_events)
-
-
-#  海外版本
-ST.url = 'https://sc.jiligaga.com/sa?project=default'
-
-
-#  ST.url = 'https://sc.jiligaga.com/sa?project=production'
-
-
-def excel_one_line_to_list_intl():
-    df = pd.read_excel('/Users/jlglqa/Downloads/cocos埋点大全.xlsx', sheet_name='埋点汇总', header=0, usecols='A',
+def excel_one_line_to_list():
+    df = pd.read_excel('/Users/jlglqa/Downloads/叽里呱啦核心埋点.xlsx', sheet_name='Sheet1', header=1, usecols='B',
                        keep_default_na=False)
 
     df_li = df.values.tolist()
@@ -47,10 +27,27 @@ def excel_one_line_to_list_intl():
     print(ST.all_events)
 
 
+#  海外版本
+# ST.url = 'https://sc.jiligaga.com/sa?project=default'
+# ST.url = 'https://sc.jiligaga.com/sa?project=production'
+
+
+# def excel_one_line_to_list_intl():
+#     df = pd.read_excel('/Users/jlglqa/Downloads/cocos埋点大全.xlsx', sheet_name='埋点汇总', header=0, usecols='A',
+#                        keep_default_na=False)
+#
+#     df_li = df.values.tolist()
+#     ST.all_events = []
+#     for s_li in df_li:
+#         ST.all_events.append(s_li[0])
+#
+#     print(ST.all_events)
+
 ST.report_path = 'report'
+
 
 addons = [
     GetData(),
-    # excel_one_line_to_list(),
-    excel_one_line_to_list_intl(),
+    excel_one_line_to_list(),
+    # excel_one_line_to_list_intl(),
 ]
