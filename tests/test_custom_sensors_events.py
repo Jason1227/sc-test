6b2aa3270053f35c05ef1e29784be2de2c7b4a62
Jason Wu <jason@jiliguala.com>
2022-07-04 11:04:56 +0800

    事件名列去重

--- tests/test_custom_sensors_events.py
+++ tests/test_custom_sensors_events.py
@@ -31,21 +31,21 @@ ST.report_path = 'report'
 # 提取excel中的数据,并转换成 '':[]的格式
 def excel_to_dict():
     df = pd.read_excel('/Users/jlglqa/Downloads/叽里呱啦核心埋点.xlsx', sheet_name=1, header=1, usecols='A, B',
-                       keep_default_na=False, dtype=str, skiprows='\t')
+                       keep_default_na=False, dtype=str)
 
-    df_li = df.values.tolist()
+    df_li = df.values.tolist()  # 转换成list
 
     ST.all_events = []
     for s_li in df_li:
-        ST.all_events.append(s_li[0])
-    #  print(ST.events_properties)
+        ST.all_events.append(s_li[0])  # 存储所有的事件名称
+    ST.all_events = list(set(ST.all_events))  # 去重
 
     ST.events_properties = {}
     for s in ST.all_events:
-        ST.events_properties[s] = []
+        ST.events_properties[s] = []  # 初始化每个事件的属性
 
     for s_li in df_li:
-        ST.events_properties[s_li[0]].append(s_li[1])
+        ST.events_properties[s_li[0]].append(s_li[1])  # 存储每个事件的属性
 
     print(ST.events_properties)
 
